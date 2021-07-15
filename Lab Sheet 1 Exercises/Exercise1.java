6dfdcec7a881c12259c03fec8afe76c60cd21e5a
Sahan Punchihewa <72688889+SahanPunchihewa@users.noreply.github.com>
2021-07-15 17:07:22 +0530

    Lab Sheet Exercise

---
+++ Lab Sheet 1 Exercises/Exercise1.java
@@ -0,0 +1,13 @@
+/*Write a program that produces the following output:
+Hello World!
+It's been nice knowing you.
+Goodbye world!*/
+
+public class Exercise1 { // Begining of class
+
+    public static void main(String[] args) { // Begining of method
+
+        System.out.println("Hello World !\nIt's been nice knowing you.\nGoodbye World !"); // End of Statement
+
+    } // End of method
+} // End of class
---
+++ Lab Sheet 1 Exercises/Exercise2.java
@@ -0,0 +1,23 @@
+/*Write a program to get the following output.
+==================================
+=      Student Information       =
+==================================
+= Name : ARDP Ranasignhe         =
+= Reg No : DIS\08\M4\1234        =
+= Address : Malabe               =
+================================== */
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=========================================\n");
+        System.out.println("=\tStudent Information\t\t=\n");
+        System.out.println("=========================================\n");
+        System.out.println("= Name\t\t:ARDP Ranasignhe\t=\n");
+        System.out.println("= Reg No\t:DIS\\08\\M4\\1234\t\t=\n");
+        System.out.println("= Address\t:Malabe\t\t\t=\n");
+        System.out.println("=========================================\n");
+    }
+
+}

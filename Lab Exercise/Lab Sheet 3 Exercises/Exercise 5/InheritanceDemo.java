c9a92b5f3ea4ed8019a06b85aa07ba8727e19fb9
Sahan Punchihewa <72688889+SahanPunchihewa@users.noreply.github.com>
2021-08-13 23:10:35 +0530

    Update InheritanceDemo.java

--- Lab Exercise/Lab Sheet 3 Exercises/Exercise 5/InheritanceDemo.java
+++ Lab Exercise/Lab Sheet 3 Exercises/Exercise 5/InheritanceDemo.java
@@ -1,12 +1,15 @@
-
-public class InheritanceDemo {  // Begining of the class
+public class InheritanceDemo { // Begining of the class 
 
 	public static void main(String[] args) { // Begining of the method
 		
-		Person p1 = new Person("Sahan", "Colombo"); // Vaalues
-		p1.showDetails(); // Display Details
+		Person p1 = new Person(); // Default Constructor 
+		p1.showDetails(); // Display Default constructor Values
+		System.out.println("");
+		
+		Person p2 = new Person("Sahan", "Colombo");
+		p2.showDetails(); // Display details
 		
 
 	} // End of the method
 
-} // end of the class
+} // End of the Class

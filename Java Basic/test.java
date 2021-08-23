20bc9e78b39a17900cfda30f4caef16db4eb8cc1
Sahan Punchihewa <72688889+SahanPunchihewa@users.noreply.github.com>
2021-08-23 20:30:30 +0530

    Add files via upload

---
+++ Java Basic/test.java
@@ -0,0 +1,30 @@
+public class test {
+ 
+    public static void main(String[] args) {
+
+        // Manual Casting
+
+    int random1 = 100;
+    
+    short random2 = (short)random1; // int to short
+
+    System.out.println("Random2 is :"+random2);
+
+    double marks_1 = 98.66; // Source Data     // Double to Float
+    float marks_2 = (float)marks_1; // destination Data
+
+    System.out.println("Marks 2 is :" +marks_2); // Output 
+
+
+        float myFloat = 65.9f;
+        int myInt = (int)myFloat;
+
+        System.out.println(+myInt);
+
+
+
+
+
+  }
+
+}

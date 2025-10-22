844f83c4d8d23fa96a3c0cf9df2bdfd4b1610c2e
SahanPunchihewa <sahannilupul18@gmail.com>
2025-10-22 21:12:02 +0530

    changes

--- Java Assignment/Question 17/Question17.java
+++ Java Assignment/Question 17/Question17.java
@@ -3,6 +3,6 @@ public class Question17{ // Beginning of the Class
     public static void main(String args[]) { // Beginning of the Method
 
 
-        System.out.print("Lamali Jeewanthi"); // Display First Name and Last Name
+        System.out.print("Sahan Punchihewa"); // Display First Name and Last Name
     } // End of the Method
 } // End of the Class
